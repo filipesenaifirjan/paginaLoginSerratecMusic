dfcbf690efd8020c819b7dda0ae29699059f4f60
filipesenaifirjan <filipemattossenai@gmail.com>
2023-02-11 13:39:51 -0300

    aula4

---
+++ js/base.js
@@ -0,0 +1,2 @@
+const URL_BASE =`https://serratec-music-manager-api.herokuapp.com`;
+
--- js/login.js
+++ js/login.js
@@ -9,6 +9,21 @@ formulario.btnEntrar.addEventListener('click' , () => {
     let usuario = new Usuario({
         email: formulario.email.value,
         senha: formulario.senha.value
-    })
+    });
+
+    autenticar(usuario.email, usuario.senha);
+//window.open('/artistas.html', '_self')
 })
-window.open('/artistas.html', '_self');
+
+function autenticar(email, senha){
+fetch(` ${URL_BASE}/api/login` , {
+    headers:{
+        "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({user: email, password: senha})
+})
+.then(responde => {
+    let token = responde.headers.get("Authorization")
+})
+}
